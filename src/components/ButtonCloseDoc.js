5015f3cf4957fb9a34cb8d1e5cea756361b47f7a
Diego Jacomel <diego@bornlogic.com>
2019-11-14 15:33:56 -0300

    Added more documentation

--- src/components/ButtonCloseDoc.js
+++ src/components/ButtonCloseDoc.js
@@ -11,21 +11,92 @@ const Spacing = styled('div')`
     margin-bottom: 40px;
 `
 
+const Text = styled('p')`
+${({ bold }) =>`
+    ${bold
+        ?
+        `
+        font-weight: bold;
+        `
+        :
+        ``
+    }
+`}`
+
 function ButtonCloseDoc() {
     return (
-        <Spacing>
-            <Title tag="h3" marginBottom="px20">
-                ButtonClose
-            </Title>
-            <Title tag="h4" marginBottom="px10">
-                Basic usage
-            </Title>
-            <ButtonClose onClick={() => alert('ButtonClose was clicked')} showButton styleguide />
-            <SyntaxHighlighter language="react" style={docco}>
-{`<ButtonClose onClick={() => alert('ButtonClose was clicked')} />
+        <>
+            <Spacing>
+                <Title tag="h3" marginBottom="px20">
+                    ButtonClose
+                </Title>
+                <Title tag="h4" marginBottom="px10">
+                    Basic usage
+                </Title>
+                <ButtonClose styleguide />
+                <SyntaxHighlighter language="react" style={docco}>
+{`<ButtonClose />
 `}
-            </SyntaxHighlighter>    
-        </Spacing>
+                </SyntaxHighlighter>
+            </Spacing>
+            <Spacing id="buttonClose">
+                <Title tag="h4" marginBottom="px20">
+                    Props
+                </Title>
+                <Spacing>
+                    <Text bold>
+                        showButton
+                    </Text>
+                    <Text>
+                        It define if the button will be shown.
+                    </Text>
+                    <Text>
+                        type: boolean
+                    </Text>
+                    <Spacing>
+                        <ButtonClose
+                            showButton={true}
+                            styleguide
+                        />
+                    </Spacing>
+                    <Spacing>
+                        <SyntaxHighlighter language="react" style={docco}>
+{`<ButtonClose
+    showButton={true}
+/>
+`}
+                        </SyntaxHighlighter>
+                    </Spacing>
+                </Spacing>
+                <Spacing>
+                    <Text bold>
+                    onClick
+                    </Text>
+                    <Text>
+                        It define what function will be triggered when the button is clicked.
+                    </Text>
+                    <Text>
+                        type: function
+                    </Text>
+                    <Spacing>
+                        <ButtonClose
+                            onClick={() => alert('You clicked on ButtonClose component')}
+                            showButton={true}
+                            styleguide
+                        />
+                    </Spacing>
+                    <Spacing>
+                        <SyntaxHighlighter language="react" style={docco}>
+{`<ButtonClose
+    onClick={() => alert('You clicked on ButtonClose component')}
+    showButton={true}
+/>
+`}
+                        </SyntaxHighlighter>
+                    </Spacing>
+                </Spacing>
+            </Spacing>
+        </>
     )
 }
 
--- src/components/CheckboxDoc.js
+++ src/components/CheckboxDoc.js
@@ -11,27 +11,134 @@ const Spacing = styled('div')`
     margin-bottom: 40px;
 `
 
+const Text = styled('p')`
+${({ bold }) =>`
+    ${bold
+        ?
+        `
+        font-weight: bold;
+        `
+        :
+        ``
+    }
+`}`
+
 function CheckboxDoc() {
     return (
-        <Spacing>
-            <Title tag="h3" marginBottom="px20">
-                Checkbox
-            </Title>
-            <Title tag="h4" marginBottom="px10">
-                Basic usage
-            </Title>
-            <FormControl inline>
-                <Checkbox
-                    name="checkbox_1"
-                    id="checkbox_1"
-                    onClick={(e) => console.log(`You ${!!e.target.checked ? '' : 'un'}selected the checkbox: ${e.target.name}`)}
-                    color="green3"
-                />
-                <Label leftSpace htmlFor="checkbox_1">
-                    Checkbox 1
-                </Label>
-            </FormControl>
-            <SyntaxHighlighter language="react" style={docco}>
+        <>
+            <Spacing>
+                <Title tag="h3" marginBottom="px20">
+                    Checkbox
+                </Title>
+                <Title tag="h4" marginBottom="px10">
+                    Basic usage
+                </Title>
+                <FormControl inline>
+                    <Checkbox
+                        name="checkbox_1"
+                        id="checkbox_1"
+                        color="blue2"
+                    />
+                    <Label leftSpace htmlFor="checkbox_1">
+                        Checkbox 1
+                    </Label>
+                </FormControl>
+                <SyntaxHighlighter language="react" style={docco}>
+{`<FormControl inline>
+    <Checkbox
+        name="checkbox_1"
+        color="blue2"
+    />
+    <Label leftSpace htmlFor="checkbox_1">
+        Checkbox 1
+    </Label>
+</FormControl>
+`}
+                </SyntaxHighlighter>
+            </Spacing>
+            <Spacing id="Checkbox">
+                <Title tag="h4" marginBottom="px20">
+                    Props
+                </Title>
+                <Spacing>
+                    <Text bold>
+                        type
+                    </Text>
+                    <Text>
+                        It specifies the checkbox type.
+                    </Text>
+                    <Text>
+                        type: string
+                    </Text>
+                </Spacing>
+                <Spacing>
+                    <Text bold>
+                        name
+                    </Text>
+                    <Text>
+                        It specifies the checkbox name.
+                    </Text>
+                    <Text>
+                        type: string, number
+                    </Text>
+                </Spacing>
+                <Spacing>
+                    <Text bold>
+                        id
+                    </Text>
+                    <Text>
+                        It specifies the checkbox id.
+                    </Text>
+                    <Text>
+                        type: string, number
+                    </Text>
+                </Spacing>
+                <Spacing>
+                    <Text bold>
+                        checked
+                    </Text>
+                    <Text>
+                        It specifies id the checkbox is checked.
+                    </Text>
+                    <Text>
+                        type: boolean
+                    </Text>
+                </Spacing>
+                <Spacing>
+                    <Text bold>
+                        onClick
+                    </Text>
+                    <Text>
+                        It define what function will be triggered when the checkbox is selected or unselected.
+                    </Text>
+                    <Text>
+                        type: function
+                    </Text>
+                </Spacing>
+                <Spacing>
+                    <Spacing>
+                        <Text bold>
+                            color
+                        </Text>
+                        <Text>
+                            It define the checkbox color.
+                        </Text>
+                        <Text>
+                            type: string
+                        </Text>
+                    </Spacing>
+                    <FormControl inline>
+                        <Checkbox
+                            name="checkbox_2"
+                            id="checkbox_2"
+                            onClick={(e) => console.log(`You {!!e.target.checked ? '' : 'un'}selected the checkbox: {e.target.name}`)}
+                            color="green3"
+                        />
+                        <Label leftSpace htmlFor="checkbox_2">
+                            Checkbox 2
+                        </Label>
+                    </FormControl>
+                    <SyntaxHighlighter language="react" style={docco}>
 {`<FormControl inline>
     <Checkbox
         name="checkbox_1"
@@ -44,8 +151,10 @@ function CheckboxDoc() {
     </Label>
 </FormControl>
 `}
-            </SyntaxHighlighter>    
-        </Spacing>
+                    </SyntaxHighlighter>
+                </Spacing>
+            </Spacing>
+        </>
     )
 }
 
